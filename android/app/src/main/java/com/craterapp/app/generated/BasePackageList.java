158a661413b9ec0b79508fc0c541a851d3edd8ce
Rihards Simanovics <rihards.s@griffin-web.studio>
2025-01-30 01:06:07 +0000

    chore: run expo run:android
    - As expected failed to build due to missing deps

---
+++ android/app/src/main/java/com/craterapp/app/generated/BasePackageList.java
@@ -0,0 +1,29 @@
+package com.craterapp.app.generated;
+
+import java.util.Arrays;
+import java.util.List;
+import org.unimodules.core.interfaces.Package;
+
+public class BasePackageList {
+  public List<Package> getPackageList() {
+    return Arrays.<Package>asList(
+        new expo.modules.application.ApplicationPackage(),
+        new expo.modules.constants.ConstantsPackage(),
+        new expo.modules.device.DevicePackage(),
+        new expo.modules.documentpicker.DocumentPickerPackage(),
+        new expo.modules.errorrecovery.ErrorRecoveryPackage(),
+        new expo.modules.filesystem.FileSystemPackage(),
+        new expo.modules.font.FontLoaderPackage(),
+        new expo.modules.imageloader.ImageLoaderPackage(),
+        new expo.modules.imagepicker.ImagePickerPackage(),
+        new expo.modules.intentlauncher.IntentLauncherPackage(),
+        new expo.modules.keepawake.KeepAwakePackage(),
+        new expo.modules.lineargradient.LinearGradientPackage(),
+        new expo.modules.localauthentication.LocalAuthenticationPackage(),
+        new expo.modules.localization.LocalizationPackage(),
+        new expo.modules.splashscreen.SplashScreenPackage(),
+        new expo.modules.storereview.StoreReviewPackage(),
+        new expo.modules.updates.UpdatesPackage()
+    );
+  }
+}
